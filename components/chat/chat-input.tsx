25d149d1f3613a9fe38bafd5987fa22198953bfb
Pablo Costa <pablo@botsfactory.io>
2024-04-08 22:06:42 -0300

    Remove "add attachment" option

--- components/chat/chat-input.tsx
+++ components/chat/chat-input.tsx
@@ -217,11 +217,11 @@ export const ChatInput: FC<ChatInputProps> = ({}) => {
         </div>
 
         <>
-          <IconCirclePlus
+          {/* <IconCirclePlus
             className="absolute bottom-[12px] left-3 cursor-pointer p-1 hover:opacity-50"
             size={32}
             onClick={() => fileInputRef.current?.click()}
-          />
+          /> */}
 
           {/* Hidden input to select files from device */}
           <Input
@@ -241,7 +241,7 @@ export const ChatInput: FC<ChatInputProps> = ({}) => {
           className="ring-offset-background placeholder:text-muted-foreground focus-visible:ring-ring text-md flex w-full resize-none rounded-md border-none bg-transparent px-14 py-2 focus-visible:outline-none disabled:cursor-not-allowed disabled:opacity-50"
           placeholder={t(
             // `Ask anything. Type "@" for assistants, "/" for prompts, "#" for files, and "!" for tools.`
-            `Ask anything. Type @  /  #  !`
+            `Ask anything.`
           )}
           onValueChange={handleInputChange}
           value={userInput}
